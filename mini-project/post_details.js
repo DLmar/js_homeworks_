e5743ed2fd83697ef3d00400da7b5ec73def7304
DLmar <dlmar2code@gmail.com>
2022-01-10 18:38:24 +0200

    ;

--- mini-project/post_details.js
+++ mini-project/post_details.js
@@ -1,21 +1,31 @@
 let url = new URL(location.href);
 let idOfPost = url.searchParams.get('id');
 
- fetch('https://jsonplaceholder.typicode.com/users/posts'+ idOfPost)
+ fetch('https://jsonplaceholder.typicode.com/posts/'+ idOfPost)
      .then(value => value.json())
      .then(value => {
          let div = document.createElement('div');
-         div.innerText = JSON.stringify(value);
+         div.classList.add('post--det');
+         div.innerHTML = JSON.stringify(value);
+         div.innerHTML = `<h4>id: ${value.id}</h4> 
+         <i>title: ${value.title}</i> 
+         <h4>body: ${value.body}</h4>`;
+
          document.body.appendChild(div);
+     fetch('https://jsonplaceholder.typicode.com/posts/'+ value.id + '/comments')
+         .then(comments => comments.json())
+         .then(comments => {
+             for (const comment of comments) {
+                 let divComment = document.createElement('div');
+                 divComment.classList.add('comments');
+                 divComment.innerHTML = JSON.stringify(comment);
+                 divComment.innerHTML = `<h4>name: ${comment.name}</h4>
+                 <p>email: ${comment.email}</p> 
+                 <h5>body: ${comment.body}</h5>`;
 
-         fetch('https://jsonplaceholder.typicode.com/posts/'+ value.id +'/comments')
-             .then(comments => comments.json())
-             .then(comments => {
-                 let div2 = document.createElement('div')
-                 div2.innerText = JSON.stringify(comments);
-                 document.body.appendChild(div2);
-
-             })
+                 document.body.appendChild(divComment);
+             }
+         })
 
      })
 
--- mini-project/user_details.js
+++ mini-project/user_details.js
@@ -6,11 +6,33 @@ fetch('https://jsonplaceholder.typicode.com/users/'+ idOfUser)
     .then(value => {
 
             let div = document.createElement('div');
-            div.innerText = JSON.stringify(value);
+            div.innerHTML = `<h4> id: ${value.id}</h4>
+            <h4>name: ${value.name}</h4>
+            <h4>username: ${value.username}</h4>
+            <i>email: ${value.email}</i>
+            <ul>address:
+                <li>street: ${value.address.street}</li>
+                <li>suite: ${value.address.suite}</li>
+                <li>city: ${value.address.city}</li>
+                <li>zipcode: ${value.address.zipcode}</li>
+                <ul>geo:
+                    <li>lat: ${value.address.geo.lat}</li>
+                    <li>lat: ${value.address.geo.lng}</li>
+                </ul>
+            </ul>
+            <h4>phone: ${value.phone}</h4>
+            <h4>website: ${value.website}</h4>
+            <ul>company:
+                <li>name: ${value.company.name}</li>
+                <li>catchPhrase: ${value.company.catchPhrase}</li>
+                <li>bs: ${value.company.bs}</li>
+            </ul>`;
+
             document.body.appendChild(div);
             
             let btn = document.createElement('button');
             btn.innerText = 'post of current user';
+            btn.classList.add('wrap--btn');
             div.appendChild(btn);
 
             btn.onclick = () => {
@@ -19,11 +41,11 @@ fetch('https://jsonplaceholder.typicode.com/users/'+ idOfUser)
                     .then(posts => {
                         for (const post of posts) {
                             let titleDiv = document.createElement('div');
-                                titleDiv.innerText = `${post.title}`;
+                                titleDiv.innerText = ` -  ${post.title}`;
 
                             let postDetailsLink = document.createElement('a');
                             postDetailsLink.innerText = '   post details page';
-                            postDetailsLink.href = 'post_details.html?id=' + post.id;
+                            postDetailsLink.href = 'post-details.html?id=' + post.id;
                             titleDiv.appendChild(postDetailsLink);
                             div.appendChild(titleDiv);
                         }
